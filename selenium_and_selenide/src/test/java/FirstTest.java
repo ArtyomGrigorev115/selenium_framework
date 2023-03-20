dcff25057102868be68f3ccac64158b7d38f5cdb
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2023-03-20 15:50:42 +0700

    ffffff

--- selenium_and_selenide/src/test/java/FirstTest.java
+++ selenium_and_selenide/src/test/java/FirstTest.java
@@ -1,15 +1,17 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.io.File;
 
 public class FirstTest {
 
-    private final static String DRIVER_PATH = "E:\\project\\testing\\test_framework\\selenium_and_selenide\\src\\test\\resources\\msedgedriver.exe";
+    private final static String DRIVER_PATH = "E:\\project\\testing\\test_framework\\selenium_and_selenide\\src\\test\\resources\\";
 
     @Test
-    public void testSearchGoogle(){
+    public void testSearchGoogle() throws InterruptedException {
         WebDriver driver;
         File file;
         file = new File(DRIVER_PATH +  "msedgedriver.exe");
@@ -17,6 +19,11 @@ public class FirstTest {
         driver = new EdgeDriver();
 
         driver.get("https://google.com");
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("ChromeDriver");
+       // driver.wait(2000);
+        searchField.submit();
+        driver.quit();
     }
 
 }
